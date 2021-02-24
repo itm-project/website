5cae91b7f812e9b87846797dd78e66fe57adf50f
fogus <pongwisanu.h@ku.th>
2021-02-24 21:02:48 +0700

    update_1

---
+++ dbconnect.php
@@ -0,0 +1,157 @@
+
+<?php
+
+function connectDB(){
+
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+
+	// $servername = "localhost";
+	// $username = "palmWeb2561";
+	// $password = "palmWeb@2561";
+
+	try {
+		$conn = new PDO("mysql:host=$servername;dbname=itmproject;charset=utf8", $username, $password);
+
+    		// $conn = new PDO("mysql:host=$servername;dbname=palmWeb2561;charset=utf8", $username, $password);
+    		// set the PDO error mode to exception
+    		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		//$conn->setAttribute(PDO::SQLSRV_ATTR_ENCODING, PDO::SQLSRV_ENCODING_UTF8);
+    		//echo "Connected successfully";
+		return $conn; 
+	}
+	catch(PDOException $e){
+    		return "Connection failed: " . $e->getMessage();
+	}
+}
+
+function selectData( $str ){
+
+	$myConDB = connectDB();
+
+	try {
+    		$stmt = $myConDB->prepare( $str ); 
+    		$stmt->execute();
+
+		$num = 0;
+		while( $result = $stmt->fetch(PDO::FETCH_ASSOC) ){
+			$num++;
+    			foreach($result as $key => $value) {
+        			//echo $key . "-----" . $value . "<br/>";
+				$data[$num][$key] = $value;
+    			}
+		}
+		$data[0]['numrow'] = $num;
+		$conn = null;
+		return $data;
+
+	}
+	catch(PDOException $e) {
+		$conn = null;
+    		return "Error: " . $e->getMessage();
+	}
+	
+	//$conn = null;
+}
+
+function selectDataOne ( $str ){
+	$myConDB = connectDB();
+
+        try {
+                $stmt = $myConDB->prepare( $str );
+                $stmt->execute();
+
+                $num = 0;
+                if( $result = $stmt->fetch(PDO::FETCH_ASSOC) ){
+                        foreach($result as $key => $value) {
+                                //echo $key . "-----" . $value . "<br/>";
+                                $data[$key] = $value;
+                        }
+                }
+		$conn = null;
+                return $data;
+
+        }
+        catch(PDOException $e) {
+		$conn = null;
+                return "Error: " . $e->getMessage();
+        }
+
+        //$conn = null;
+}
+
+function selectDataArray( $str ){
+
+	$myConDB = connectDB();
+
+	try {
+    		$stmt = $myConDB->prepare( $str ); 
+		$stmt->execute();
+		    
+		$bucket = "";
+
+		while( $result = $stmt->fetch(PDO::FETCH_ASSOC) ){
+
+			$set = "";
+    			foreach($result as $key => $value) {
+				$set .= "\"".$key."\":\"".$value."\",";
+			    }
+
+			$set = rtrim($set,',');
+			$bucket .= "{".$set."},";
+		}
+		$bucket = rtrim($bucket,',');
+		
+		$resultJson = "[".$bucket."]";
+
+		$conn = null;
+		return $resultJson;
+
+	}
+	catch(PDOException $e) {
+		$conn = null;
+    		return "Error: " . $e->getMessage();
+	}
+	
+	//$conn = null;
+}
+
+
+function updateData( $str ){
+	$myConDB = connectDB();
+	
+	try {
+                $stmt = $myConDB->prepare( $str );
+                $stmt->execute();
+
+		$conn = null;
+                return "OK";
+
+        }
+        catch(PDOException $e) {
+		$conn = null;
+                return "Error: " . $e->getMessage();
+        }
+}
+
+function addinsertData( $str ){
+	$myConDB = connectDB();
+	
+	try {
+                $stmt = $myConDB->prepare( $str );
+                $stmt->execute();
+
+		$conn = null;
+                return $myConDB->lastInsertId();
+
+        }
+        catch(PDOException $e) {
+		$conn = null;
+                return "Error: " . $e->getMessage();
+        }
+}
+
+
+?>
+
---
+++ src/view/component/blank.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include_once("../layout/header.php") ?>
+
+                <!-- Begin Page Content -->
+                <div class="container-fluid">
+
+                    <!-- Page Heading -->
+                    <h1 class="h3 mb-4 text-gray-800">Blank Page</h1>
+
+                </div>
+                <!-- /.container-fluid -->
+
+            </div>
+            <!-- End of Main Content -->
+
+<?php include_once("../layout/footer.php") ?>
